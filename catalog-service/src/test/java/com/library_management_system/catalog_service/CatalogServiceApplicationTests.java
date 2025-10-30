1003cac45e9e68235d38478f4aad7d4b59374d05
jphouli <jordan_houlihan@yahoo.com>
2025-10-30 15:57:30 -0400

    Added initialized services from Spring Boot Init

---
+++ catalog-service/src/test/java/com/library_management_system/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library_management_system.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
